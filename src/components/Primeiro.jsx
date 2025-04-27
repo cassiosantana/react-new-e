bd2d6bfcee32356f097de05af7b40216051b6b53
Cassio Santana <cassioo.roger@gmail.com>
2025-04-27 09:34:16 -0300

    primeiro componente

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,9 @@
+// export default function() {
+//     return <h1>Primeiro</h1>
+// }
+
+// export default () => {
+//     return <h1>Primeiro</h1>
+// }
+
+export default () => <h1>Primeiro</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
+import Primeiro from "./components/Primeiro";
 
 const element = <h1>Projeto React</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+// root.render(<h1>Projeto React</h1>);
+// root.render(element);
+
+root.render(<Primeiro/>);
